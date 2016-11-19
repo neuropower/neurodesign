f96e1b5d85d5204fcbee78c5d557119d76b08f50
jokedurnez <joke.durnez@gmail.com>
2016-11-18 16:36:12 -0800

    documentation

--- docs/conf.py
+++ docs/conf.py
@@ -197,10 +197,10 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-  ('index', 'neurodesign.tex', u'neurodesign Documentation',
-   u'Joke Durnez', 'manual'),
-]
+# latex_documents = [
+#   ('index', 'neurodesign.tex', u'neurodesign Documentation',
+#    u'Joke Durnez', 'manual'),
+# ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.

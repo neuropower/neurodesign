df1c3b588f97ffd9f1f3fae51f4e58a19eaff999
jokedurnez <joke.durnez@gmail.com>
2017-02-20 16:28:26 -0800

    documentation pass

--- docs/conf.py
+++ docs/conf.py
@@ -250,7 +250,7 @@ texinfo_documents = [
 
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
 for mod_name in MOCK_MODULES:
    sys.modules[mod_name] = mock.Mock()
 

4bcabbf41a2f1eadcaa0eb59f6a33b5b10891a9e
jokedurnez <joke.durnez@gmail.com>
2018-02-12 13:06:46 -0800

    direct loading of experiment, design and optimisation

--- source/src/__init__.py
+++ source/src/__init__.py
@@ -4,3 +4,16 @@ This module contains code to find an optimal experimental design for fMRI experi
 .. moduleauthor:: Joke Durnez <joke.durnez@gmail.com>
 
 '''
+
+from neurodesign import design, experiment, optimisation
+
+from .info import (
+    __version__,
+    __author__,
+    __license__,
+    __email__,
+    __status__,
+    __url__,
+    __packagename__,
+
+)

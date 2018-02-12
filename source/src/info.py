3ce64bd781b59fffe42a59155a6f81f641647653
jokedurnez <joke.durnez@gmail.com>
2018-02-12 13:07:08 -0800

    add package information

---
+++ source/src/info.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Base module variables
+"""
+
+__version__ = '0.2.00'
+__author__ = 'Joke Durnez'
+__license__ = 'MIT'
+__email__ = 'joke.durnez@gmail.com'
+__status__ = 'Prototype'
+__url__ = 'https://www.neuropowertools.org'
+__packagename__ = 'neurodesign'
